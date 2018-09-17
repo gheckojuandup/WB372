d06ca0ffe0c9eaaf8613114759152617eb1cb686
gheckojuandup <39627411+gheckojuandup@users.noreply.github.com>
2018-09-17 15:13:15 +0200

    Added new code for romberg function

--- romberg.py
+++ romberg.py
@@ -1,57 +1,118 @@
-import numpy
+import numpy as np
+import math
 
-def romberg( f, a, b, n ):
-    """Estimate the integral of f(x) from a to b using Romberg Integration.
+def results_mtx(function, interval, resmat):
+    """Print the Romberg results in the form Steps, Stepsize and Results"""
+    i = j = 0
+    print('Romberg integration from ' + str(interval[0]) + ' to ' + str(interval[1]))
+    print
+    for i in xrange(len(resmat)):
+        print('Steps: ' + str((2**i)))
+	print('StepSize: ' + str((interval[1]-interval[0])/(2.**i)))
+	newresults = []
+        for j in xrange(i+1):
+	    newresults.append(resmat[i][j])
+	print('Results: ' + str(newresults))
+	print('')
+    print('')
+    print('The final result is ' + str(resmat[i][j]))
+    print('after ' + str(2**(len(resmat)-1)+1) +' function evaluations.')
 
-    USAGE:
-        r = romberg( f, a, b, n )
 
-    INPUT:
-        f       - function to integrate,
-        [a, b]  - the interval of integration,
-        n       - number of levels of recursion
-
-    OUTPUT:
-        numpy float array - Romberg integration array; most accurate
-                            answer should be at bottom of right-most column,
-
-    NOTES:
-        Based on an algorithm in "Numerical Mathematics and Computing"
-        4th Edition, by Cheney and Kincaid, Brooks-Cole, 1999.
-
-    AUTHOR:
-        Jonathan R. Senning <jonathan.senning@gordon.edu>
-        Gordon College
-        February 17, 1999
-        Converted to Python August 2008
+def romberg_diff(b, c, k):
     """
+    Compute the differences for the Romberg quadrature corrections.
+    """
+    tmp = 4.0**k
+    return (tmp * c - b)/(tmp - 1.0)
 
-    r = numpy.array( [[0] * (n+1)] * (n+1), float )
-    h = b - a
-    r[0,0] = 0.5 * h * ( f( a ) + f( b ) )
 
-    powerOf2 = 1
-    for i in xrange( 1, n + 1 ):
+def difftrap(function, interval, numtraps):
+    """
+    Perform part of the trapezoidal rule to integrate a function.
+    """
+    if numtraps <= 0:
+        raise ValueError("numtraps must be > 0 in difftrap().")
+    elif numtraps == 1:
+        return 0.5*(function(interval[0])+function(interval[1]))
+    else:
+        numtosum = numtraps/2
+        h = float(interval[1]-interval[0])/numtosum
+        lox = interval[0] + 0.5 * h
+        points = lox + h * np.arange(numtosum)
+        s = np.sum(function(points), axis=0)
+        return s
 
-        # Compute the halved stepsize and use this to sum the function at
-        # all the new points (in between the points already computed)
 
-        h = 0.5 * h
+def vectorize(func, args=(), vec_func=False):
+    """Vectorize the call to a function.
+    This is an internal utility function used by `romberg` 
+    to create a vectorized version of a function.
+    If `vec_func` is True, the function `func` is assumed to take vector
+    arguments.
+    """
+    if vec_func:
+        def vfunc(x):
+            return func(x, *args)
+    else:
+        def vfunc(x):
+            if np.isscalar(x):
+                return func(x, *args)
+            x = np.asarray(x)
+            # call with first point to get output type
+            y0 = func(x[0], *args)
+            n = len(x)
+            dtype = getattr(y0, 'dtype', type(y0))
+            output = np.empty((n,), dtype=dtype)
+            output[0] = y0
+            for i in xrange(1, n):
+                output[i] = func(x[i], *args)
+            return output
+    return vfunc
 
-        sum = 0.0
-        powerOf2 = 2 * powerOf2
-        for k in xrange( 1, powerOf2, 2 ):
-            sum = sum + f( a + k * h )
 
-        # Compute the composite trapezoid rule for the next level of
-        # subdivision.  Use Richardson extrapolation to refine these values
-        # into a more accurate form.
+def romberg(f, a, b, tol=1.0e-6):
+    """
+	I, n_panels = romberg(f, a, b, tol=1.0e-6)
+	Return the integral from a to b of f(x), by Romberg integration,
+	as well as the number of panels used.
+    """
+    if np.isinf(a) or np.isinf(b):
+        raise ValueError("Romberg integration only available "
+                         "for finite limits.")
+    show = True
+    vec_func = False
+    vfunc = vectorize(f, (), vec_func=vec_func)
+    n = 1
+    interval = [a, b]
+    intrange = b - a
+    ordsum = difftrap(vfunc, interval, n)
+    result = intrange * ordsum
+    resmat = [[result]]
+    err = np.inf
+    last_row = resmat[0]
+    for i in xrange(1, 10+1):
+        n *= 2
+        ordsum += difftrap(vfunc, interval, n)
+        row = [intrange * ordsum / n]
+        for k in xrange(i):
+            row.append(romberg_diff(last_row[k], row[k], k+1))
+        result = row[i]
+        lastresult = last_row[i-1]
+        if show:
+            resmat.append(row)
+        err = abs(result - lastresult)
+        if err < tol:
+            break
+        last_row = row
 
-        r[i,0] = 0.5 * r[i-1,0] + sum * h
+    if show:
+        results_mtx(vfunc, interval, resmat)
+    return result, str(2**(len(resmat)-1)+1)
 
-        powerOf4 = 1
-        for j in xrange( 1, i + 1 ):
-            powerOf4 = 4 * powerOf4
-            r[i,j] = r[i,j-1] + ( r[i,j-1] - r[i-1,j-1] ) / ( powerOf4 - 1 )
 
-    return r
+if __name__ == "__main__":
+
+	def myfunc(x):
+		return (x**2)*math.pi
+	print(romberg(myfunc, 0, 1))
